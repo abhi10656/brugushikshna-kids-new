9e6c5517a028a572238bdd9ba88257327be38f96
unknown <megha@immensphere.com>
2022-07-07 14:36:29 +0530

    pushing

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ app.get('/privacy', function(req, res) {
 app.get('/nursery', function(req, res) {
   res.render('innerpages/nursery');
 });
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
 
 // about page
 app.get('/about', function(req, res) {
