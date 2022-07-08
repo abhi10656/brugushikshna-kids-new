cf5f4b779ec1c65d1ea602db0ff418c2bde80cfa
unknown <megha@immensphere.com>
2022-07-08 18:43:20 +0530

    pushing

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
+app.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
 // about page
 app.get('/about', function(req, res) {
   res.render('pages/about');
